e162f8b6f22f5ed75d294f8063c190d64c28288a
Amina Kobenova <56433807+ak7588@users.noreply.github.com>
2021-10-09 14:08:51 +0400

    Add second interaction

---
+++ ProjectTwo/Assets/Scripts/ConveyerBeltInteraction.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyerBeltInteraction : MonoBehaviour
+{
+    public GameObject blueGift;
+    public AudioSource playSound;
+    public GameObject grayBox;
+
+    private void Start()
+    {
+        blueGift.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("GrayBox"))
+        {
+            other.gameObject.SetActive(false);
+            blueGift.SetActive(true);
+            playSound.Play();
+        }
+    }
+}
