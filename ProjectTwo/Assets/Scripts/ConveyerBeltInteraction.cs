7771fff9a4f4afd609cb829ffd68103858f0de02
Amina Kobenova <56433807+ak7588@users.noreply.github.com>
2021-10-11 13:43:35 +0400

    Modify code and scene

--- ProjectTwo/Assets/Scripts/ConveyerBeltInteraction.cs
+++ ProjectTwo/Assets/Scripts/ConveyerBeltInteraction.cs
@@ -7,21 +7,36 @@ public class ConveyerBeltInteraction : MonoBehaviour
     public GameObject blueGift;
     public AudioSource playSound;
     public GameObject grayBox;
-    private MasterScript MasterScript;
+    public GameObject arrowNav1;
+    public GameObject arrowNav2;
+    public GameObject arrowNav3;
+    public GameObject arrowNav4;
+    public GameObject arrowNav5;
+    //private MasterScript MasterScript;
 
     private void Start()
     {
         blueGift.SetActive(false);
+        arrowNav1.SetActive(true);
+        arrowNav2.SetActive(true);
+        arrowNav3.SetActive(false);
+        arrowNav4.SetActive(false);
+        arrowNav5.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("GrayBox"))
         {
+            arrowNav1.SetActive(false);
+            arrowNav2.SetActive(false);
+            arrowNav3.SetActive(true);
+            arrowNav4.SetActive(true);
+            arrowNav5.SetActive(true);
+
             other.gameObject.SetActive(false);
             blueGift.SetActive(true);
             playSound.Play();
-            MasterScript.taskOneCompleted = true;
         }
     }
 }
