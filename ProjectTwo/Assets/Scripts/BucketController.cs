7c91f5917fc12415d3ea0d559c86f4954b18f97f
Amina Kobenova <56433807+ak7588@users.noreply.github.com>
2021-10-11 15:44:35 +0400

    Apply prof's feedback

--- ProjectTwo/Assets/Scripts/BucketController.cs
+++ ProjectTwo/Assets/Scripts/BucketController.cs
@@ -7,16 +7,33 @@ public class BucketController : MonoBehaviour
     public MeshRenderer bucketRenderer;
     public AudioSource playSound;
     private int count = 0;
-    private MasterScript MasterScript;
+    public GameObject arrowNav3;
+    public GameObject arrowNav4;
+    public GameObject arrowNav5;
+    public GameObject arrowNav6;
+    public GameObject arrowNav7;
+    public GameObject arrowNav8;
+
+    private void Start()
+    {
+        arrowNav7.SetActive(false);
+        arrowNav8.SetActive(false);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Gift")) {
+            other.gameObject.GetComponent<XRBaseInteractable>().enabled = false;
             count += 1;
             if (count == 3)
             {
                 playSound.Play();
-                MasterScript.taskTwoCompleted = true;
+                arrowNav3.SetActive(false);
+                arrowNav4.SetActive(false);
+                arrowNav5.SetActive(false);
+                arrowNav6.SetActive(false);
+                arrowNav7.SetActive(true);
+                arrowNav8.SetActive(true);
             }
         }
     }
--- ProjectTwo/Assets/Scripts/StartEngine.cs
+++ ProjectTwo/Assets/Scripts/StartEngine.cs
@@ -5,15 +5,45 @@ using UnityEngine;
 public class StartEngine : MonoBehaviour
 {
     public GameObject loc;
+    public GameObject keyCopy;
+    public GameObject key;
     public float speed = 200.0f;
     public AudioSource santaVoice;
+    public AudioSource final;
+    public GameObject arrowNav7;
+    public GameObject arrowNav8;
+    private bool taskCompleted = false;
+    private bool isPlaying = false;
+
+    private void Start()
+    {
+        keyCopy.SetActive(false);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Key"))
         {
             transform.Rotate(Vector3.right * speed * Time.deltaTime);
+            arrowNav7.SetActive(false);
+            arrowNav8.SetActive(false);
+            //keyCopy.SetActive(true);
+            //key.SetActive(false);
+            taskCompleted = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (taskCompleted && !isPlaying)
+        {
             santaVoice.Play();
+            final.Play();
+            isPlaying = true;
+        }
+
+        if (taskCompleted && isPlaying) {
+            final.Play();
         }
     }
 }
